4741f1ca1a47548e9ddfa31d867060a64290b555
Mystic <nicholelin3929@gmail.com>
2024-02-07 16:20:02 -0500

    Queens problem set up

--- Cpp_Lab/Lab_4.cpp
+++ Cpp_Lab/Lab_4.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 int main(){
-    cout<<"First one"<<endl;
-    here: cout<<"Hello! ";
+    int r = 0, c = 0;
+    int solutionCounter = 0;
+    int q[0][0] = {0};
 
-    while(true){
-        goto here;
-    }
+NextColumn:
+    // column increment code
+
+NextRow:
+    // row inc rement code
+
+BackTrack:
+    // backTrack Code
+
+PrintSotution:
+    // printSoIution Code 
+          
     return 0;
 }
