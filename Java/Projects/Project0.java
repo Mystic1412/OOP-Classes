d7f78033bd20ef9f65b9a125051394c54ce5976f
Mystic <nicholelin3929@gmail.com>
2024-02-21 11:18:50 -0500

    starting lab 6

--- Java/Projects/Project0.java
+++ Java/Projects/Project0.java
@@ -2,8 +2,22 @@ import javax.swing.*;
 
 public class Project0 {
     //user input a sentence, using a JOptionPane.showInputDialog()
+    String UserSentence = JOptionPane.showInputDialog("Enter a sentence");
     //count uppercase E and lowercased e, using charAt method
-    //Using a JOptionPane.showMessageDialog(), tell the user how many upper and lower case e's were in the string.
-    //Repeat aka loop it until the user types "STOP"
+    //Using a JOptionPane.showMessageDialog(), 
+    //tell the user how many upper and lower case e's were in the string.
+    //Repeat aka loop it until the user types "STOP" 27
 
+    public static int Counte(String sentence){
+        while(true){
+            if(sentence.equalsIgnoreCase("STOP")){
+                System.exit(0);
+            }
+            
+            
+
+        }
+
+        
+    }
 }
--- Java/Self Learning/Learning.java
+++ Java/Self Learning/Learning.java
@@ -7,5 +7,18 @@ public class Learning {
         double pi = 3.1415926; //8bytes
         System.out.print("The numbers of pi is "+pi);
 
+        //GUI using javax.swing.JOptionPane
+        String name= JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+ name);
+
+        //Doing integers
+        int Age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age")); 
+        //(data type).parseInt changes the JOptionPane from a string to a integer
+        JOptionPane.showMessageDialog(null, "You are "+Age+" years old!");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your age")); 
+        //(data type).parseDouble changes the JOptionPane from a string to a double
+        JOptionPane.showMessageDialog(null, "You are "+height+" cm tall");
+
     }
 }
