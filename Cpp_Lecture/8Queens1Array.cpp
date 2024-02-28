cc76ad31d0c5adbe3e241c7c14ae8c0c6ee6089e
Mystic <nicholelin3929@gmail.com>
2024-02-28 13:31:29 -0500

    java lab 10 javadoc

--- Cpp_Lecture/8Queens1Array.cpp
+++ Cpp_Lecture/8Queens1Array.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-using namespace std;
 #include <cmath>
+using namespace std;
 
 bool ok(int arr[]){
-    //for each column (or queen)
-    for (int c=0; c<8; c++){
-        //Look at the columns before
-        for(int i=0; i<c; i++){
-            if(arr[c]==arr[i]){
+    for (int col=0; col<8;col++){
+        for(int i=0; i<8; i++){
+            if(arr[i]==arr[col]){ //row check
                 return false;
-            }
-            if((c-i)==abs(arr[c]-arr[i])){
+            }if(arr[i]==abs(arr[col]-arr[col])){ //both diagonals 
                 return false;
             }
         }
@@ -19,31 +16,18 @@ bool ok(int arr[]){
 }
 
 int main(){
-    int q[8]={};
-    for(int i0 = 0; i0<10;i0++){
-        
-        for(int i1 =0; i1<10;i1++){
-            
-            for(int i2 =0; i2 <10;i2++){
-                
-                for(int i3 =0; i3<10;i3++){
-                   
-                    for(int i4 =0; i4<10;i4++){
-                        
-                        for(int i5 =0; i5<10;i5++){
-                            
-                            for(int i6=0; i6<10;i6++){
-                                
-                                for(int i7=0; i7<10;i7++){
-                                
-                                }
-                            }
-                        }
-                    }
-                }   
-            }   
-        }     
-    }
+    int board[8], int index=0;
+    board[index]=1;
+NextCol:    //next col
+    index++;
 
+if(ok(board)){
+    goto NextCol;
+}
+
+BackTrack:
+
+goto NextCol;
+    //print
     return 0;
 }
