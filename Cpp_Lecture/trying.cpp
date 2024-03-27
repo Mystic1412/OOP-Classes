9f761971881c25ba3257f11e6ae1c21cda0c3663
Mystic <nicholelin3929@gmail.com>
2024-03-26 22:09:03 -0400

    testing pointer b array

--- Cpp_Lecture/trying.cpp
+++ Cpp_Lecture/trying.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-#include<cstdlib>
-#include <cmath>
 using namespace std;
-int main(){
-
-  return 0;
-}//main method
-             
+int main( )
+{ typedef int my_2darray[1][1];
+my_2darray b[3][2];
+cout<<sizeof(b)<<endl;
+cout<<sizeof(b+0)<<endl;
+cout<<sizeof(*(b+0))<<endl;
+// the next line prints 0012FF4C
+cout<<"The address of b is: "<<b<<endl;
+cout<<"The address of b+1 is: "<<b+1<<endl;
+cout<<"*(b+1) is: "<<*(b+1)<<endl<<endl;
+cout<<"The address of &b is: "<<&b<<endl;
+cout<<"The address of &b+1 is: "<<&b+1<<endl<<endl;
+return 0;
+}
