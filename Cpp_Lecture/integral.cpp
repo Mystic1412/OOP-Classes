6241541406025dc28133fd7e0b6835806fbc9a52
Mystic <nicholelin3929@gmail.com>
2024-04-17 15:10:11 -0400

    finished integral problem

--- Cpp_Lecture/integral.cpp
+++ Cpp_Lecture/integral.cpp
@@ -4,25 +4,33 @@ using namespace std;
 
 typedef double (*func)(double);
 
-double Equation(double x){
-    double xr = x*x+4;
-    return xr;
-}
-
-double integral(double a, double b){
-    func f = Equation;
-    double sum=0, x=a, d=(b-a)/4; 
+double integrate(func f, double a, double b){
+    double sum=0, x=a, d=.000001; 
     while(x<b){
         sum += f(x)*d;
-        x= x+d;
-    }
+        x += d;
+    }   
     return sum;
+}
+
+double line(double x){
+   return x;
+}
+
+double square(double x){
+   return x*x;
+}
+
+double cube(double x){
+   return x*x*x;
 }
 
 
 int main(){
     
-    cout<<integral(1.0,5.0)<<endl;
+    cout << "The integral of f(x)=x between 1 and 5 is: " << integrate(line, 1, 5) << endl;     // 12
+    cout << "The integral of f(x)=x^2 between 1 and 5 is: " << integrate(square, 1, 5) << endl; // 41.3333
+    cout << "The integral of f(x)=x^3 between 1 and 5 is: " << integrate(cube, 1, 5) << endl;   // 156
 
     return 0;
 }
--- Cpp_Lecture/test.cpp
+++ Cpp_Lecture/test.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  return 0;
+
+typedef double (*func)(double);
+
+double Equation(double x){
+    double xr = (2*x)-5;
+    return xr;
+}
+
+double integral(double a, double b){
+    func f = Equation;
+    double sum=0, x=a, d=2; 
+    while(x<b){
+        sum += f(x)*d;
+        x= x+d;
+    }
+    return sum;
+}
+
+
+int main(){
+    
+    cout<<integral(-6.0,4.0)<<endl;
+
+    return 0;
 }
